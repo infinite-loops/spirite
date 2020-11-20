c052d50714f8f128ceb8b62e23b867a680fa31a4
Ivan Taranin <ivan.taranin@infiniteloops.de>
2020-11-20 09:46:48 +0100

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Ivan Taranin (john) on 20.11.20.
+ * [ivan.taranin@infiniteloops.de]
+ */
+function setup() {
+    createCanvas(720, 560);
+    noStroke();
+    noLoop();
+}
+
+function draw() {
+    drawCircle(width / 2, 280, 6);
+}
+
+function drawCircle(x, radius, level) {
+    const tt = (126 * level) / 4.0;
+    fill(tt);
+    ellipse(x, height / 2, radius * 2, radius * 2);
+    if (level > 1) {
+        level = level - 1;
+        drawCircle(x - radius / 2, radius / 2, level);
+        drawCircle(x + radius / 2, radius / 2, level);
+    }
+}
+
